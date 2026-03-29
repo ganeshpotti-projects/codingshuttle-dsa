4234feeeefd3809d28469056c304b13d185961de
ganeshpotti-projects <ganeshpotti.projects@gmail.com>
2026-03-29 23:54:37 +0530

    DSA | module-5: backtracking thory

---
+++ _05_backtracking/src/_5_2_RatInMaze.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class _5_2_RatInMaze {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] mat = {
+                {1, 1, 1, 0}
+        }
+    }
+}
